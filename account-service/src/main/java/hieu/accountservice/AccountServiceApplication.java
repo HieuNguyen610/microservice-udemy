5af7d6d4201651d6bcf322f3ce07478b031ccef8
hieunm2 <hieunm2@vnext.vn>
2025-04-03 09:53:14 +0700

    account-service commit

--- account-service/src/main/java/hieu/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/hieu/accountservice/AccountServiceApplication.java
@@ -1,13 +1,13 @@
-package hieu.microserviceudemy;
+package hieu.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroserviceUdemyApplication {
+public class AccountServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MicroserviceUdemyApplication.class, args);
+        SpringApplication.run(AccountServiceApplication.class, args);
     }
 
 }
--- account-service/src/test/java/hieu/accountservice/AccountServiceApplicationTests.java
+++ account-service/src/test/java/hieu/accountservice/AccountServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package hieu.microserviceudemy;
+package hieu.accountservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MicroserviceUdemyApplicationTests {
+class AccountServiceApplicationTests {
 
     @Test
     void contextLoads() {
