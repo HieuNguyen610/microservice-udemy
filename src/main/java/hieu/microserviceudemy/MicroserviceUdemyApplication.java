d741c53b4793fc239124cca9cdb1a9cfd8e8f6cc
hieunm2 <hieunm2@vnext.vn>
2025-04-03 09:48:42 +0700

    unit commit

---
+++ src/main/java/hieu/microserviceudemy/MicroserviceUdemyApplication.java
@@ -0,0 +1,13 @@
+package hieu.microserviceudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceUdemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceUdemyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hieu/microserviceudemy/MicroserviceUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package hieu.microserviceudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUdemyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
