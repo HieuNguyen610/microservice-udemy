22293688c2c5514e7930e6c94e78f81bbb1e61a4
hieunm2 <hieunm2@vnext.vn>
2025-04-04 16:52:42 +0700

    gateway service

--- gateway-service/src/main/java/hieu/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/hieu/gatewayservice/GatewayServiceApplication.java
@@ -1,9 +1,12 @@
 package hieu.gatewayservice;
 
+import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
